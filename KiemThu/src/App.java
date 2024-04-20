a95c95271bead57881b68109c53357f8717c47ab
atmcorgi <124545839+atmcorgi@users.noreply.github.com>
2024-04-20 13:01:21 +0000

    add

---
+++ KiemThu/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println(sayHello());
+    }
+
+    public static String sayHello() {
+        return "Haha";
+    }
+}
